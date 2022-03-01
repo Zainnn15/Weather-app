61db787640095ea0a6032b321f4c86d2a1e6a96c
Zain <Zainnn15.q@gmail.com>
2022-02-28 22:28:00 -0500

    f

--- assign2/src/main.js
+++ assign2/src/main.js
@@ -75,9 +75,7 @@ function Main() {
             <div className="row">
                 <nav className="navbar navbar-expand-lg bg-dark">
                     <a className="navbar-brand" href="#">Weather App_v2</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" aria-label="Toggle navigation"></button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
